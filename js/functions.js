a4cc1b25ae485d1e1d02ff0d3c53072f4e01beeb
akazad1235 <akazad914@gmail.com>
2020-08-04 14:52:56 +0600

    pin matcher added

---
+++ js/functions.js
@@ -0,0 +1,10 @@
+function pinNotMatch(receive){
+    var dontMatch = document.getElementById('pin-not-match')
+                    dontMatch.style.display=receive
+}
+function pinMatch(rec){
+    var dontMatch = document.getElementById('pin-match')
+                    dontMatch.style.display=rec
+}
+
+
