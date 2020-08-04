e95599741052bfae0d95e2672077199171af0ca0
akazad1235 <akazad914@gmail.com>
2020-08-04 21:56:49 +0600

    Pin-matcher finished

--- js/apps.js
+++ js/apps.js
@@ -1,46 +1,35 @@
 
         const pinGenarete = document.getElementById('pin-generate-btn')
         pinGenarete.addEventListener('click', function(event){
-           var newNumber = Math.floor(1000+Math.random()*9000)
+           var newNumber = Math.floor(1000+Math.random()*9999)
 
 
            document.getElementById('pin-display').value = newNumber
         }) 
 
+        //keypad
         const numberBtn = document.getElementsByClassName('button')
             for (let i = 0; i < numberBtn.length; i++) {
                 numberBtn[i].addEventListener('click', function(e){
                     var num = this.id
                     number(num)
                    
-                })
-
-
-               
-                
+                })   
             }
-        function number(num){
-                     var  oldValue = document.getElementById('number-display').value
-                   var output = oldValue+num
-                   var ConvertNumber = parseInt(output)
-                   document.getElementById('number-display').value = ConvertNumber
- 
-                 
-        }
 
-            var testSubmit = document.getElementById('submit')
-            testSubmit.addEventListener('click', function(e){
+            //for submit button
+            var submitBtn = document.getElementById('submit')
+            submitBtn.addEventListener('click', function(e){
 
-              var getData =   document.getElementById('number-display').value 
-              var getDataNumber = parseInt(getData)
+              var keypadDisplay =   document.getElementById('number-display').value 
+              var keypadDisplayNumber = parseInt(keypadDisplay)
               
-              var pinData =   document.getElementById('pin-display').value 
-              var pinDataNumber = parseInt(pinData)
+              var pinGenareteDiplay =   document.getElementById('pin-display').value 
+              var pinGenareteDiplayNumber = parseInt(pinGenareteDiplay)
 
-               if(getDataNumber == pinDataNumber){   
+               if(keypadDisplayNumber == pinGenareteDiplayNumber){   
                 var number = document.getElementsByClassName('notify')
                 for (let i = 0; i < number.length; i++) {
-                    const element = number[i];
                     if (number[i] == number[1]) {
                         pinMatch('block')   
                     }
@@ -48,28 +37,24 @@
                         pinNotMatch('none')  
                     }
                 }
-
-                }else if(isNaN(getDataNumber) || isNaN(pinDataNumber)){
+                }else if(isNaN(keypadDisplayNumber) || isNaN(pinGenareteDiplayNumber)){
                     alert('Please Enter Your Pin')
                 }else{
                 var number = document.getElementsByClassName('notify')
                 for (let i = 0; i < number.length; i++) {
-                    const element = number[i];
                     if (number[i] == number[0]) {
                         pinNotMatch('block')
                     }
                     if (number[i]== number[1]) {
-                        
                         pinMatch('none')  
                     }
                 }   
                 }
                 
             })
-            //operator button action 
+            //operator button action back-space & clear
             var operator = document.getElementsByClassName('operator')
             for (let i = 0; i < operator.length; i++) {
-                const element = operator[i];
                 operator[i].addEventListener('click', function(event){
                     var output = document.getElementById('number-display').value
                     var outputs = output.toString()
--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,18 @@
-function pinNotMatch(receive){
-    var dontMatch = document.getElementById('pin-not-match')
-                    dontMatch.style.display=receive
+function number(num){
+    var  oldValue = document.getElementById('number-display').value
+  var output = oldValue+num
+  var ConvertNumber = parseInt(output)
+  document.getElementById('number-display').value = ConvertNumber
+
+
 }
-function pinMatch(rec){
+function pinNotMatch(data){
+    var dontMatch = document.getElementById('pin-not-match')
+        dontMatch.style.display = data
+}
+function pinMatch(data){
     var dontMatch = document.getElementById('pin-match')
-                    dontMatch.style.display=rec
+        dontMatch.style.display = data
 }
 
 
